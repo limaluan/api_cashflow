883ea8c80018d59102d0462725e121acf2145459
Luan Lima <limaluan.dev@gmail.com>
2023-09-12 14:45:55 -0600

    feat: Adds a Create User Controller...
      - adds: Create User route
      - adds: All project settings and libs

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import { userRepository } from "../repositories/userRepository";
+import { BadRequestError } from "../helpers/api-errors";
+
+import bcrypt from "bcrypt";
+
+export class UserController {
+  async create(req: Request, res: Response) {
+    const { email, password, name } = req.body;
+
+    const userExists = await userRepository.findOneBy({ email });
+
+    if (userExists) throw new BadRequestError("Email já existe");
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = userRepository.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+
+    await userRepository.save(newUser);
+
+    const { password: _, ...user } = newUser;
+    
+    return res.status(201).json(user);
+  }
+}
